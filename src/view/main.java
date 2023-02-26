b52feb07e29026eb30d095f5d7413e3c8d052380
Jorge  Augusto de Oliveira Rocha <jorgeaugust76@gmail.com>
2023-02-26 16:28:51 -0300

    correção

--- src/view/main.java
+++ src/view/main.java
@@ -4,9 +4,9 @@ public class main {
 	public static void main( String[] args) {
 		modulos mod = new modulos();
 		int n1 = 5;
-		int n2 = 5;
+		int n2 = 6;
 		int mul = mod.multiplica(n1, n2);
 		
-		System.out.println(n1 + " + " + n2 + " = "+ mul);
+		System.out.println(n1 + " x " + n2 + " = "+ mul);
 	}
 }
